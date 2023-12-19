5ed90a8cac9dfb0f98ce39e50004d6e1345a0850
Cohan Le <vietle632@gmail.com>
2023-12-19 15:09:58 +0700

    update 2

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,7 @@
 import 'dart:html';
 
 void main() {
-  //Lấy phần tử có id = idxinchao
   var xinchao = querySelector('#App_shopItemButton_23FO1');
 
-  //Khi load trang thiết lập dòng chữ
-
-  //Bấm vào dòn chữ thì viết ra log
   xinchao?.addEventListener('click', (e) => print('Bấm vào dòng chữ'));
 }
